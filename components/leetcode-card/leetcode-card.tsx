6d39212696950bd0770c74d6c95143e840f921d0
Suhel Khan <111196189+Suhelkhan12@users.noreply.github.com>
2024-12-30 22:04:17 +0530

    Update leetcode-card.tsx

--- components/leetcode-card/leetcode-card.tsx
+++ components/leetcode-card/leetcode-card.tsx
@@ -16,10 +16,10 @@ import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 import { UserData } from "@/lib/types";
 import { Button } from "../ui/button";
 
-const TOTAL=3399;
-const EASY=845;
-const MEDIUM=1772;
-const HARD=782;
+const TOTAL=3405;
+const EASY=846;
+const MEDIUM=1775;
+const HARD=784;
 
 export default function LeetCodeStatsCard({ data }: { data: UserData | null }) {
   const handleDownload = () => {
